0461500d37c9f34cf88779c8835dff67f9d2f857
barbarazpc <barbara.zita.peters@couto.de>
2023-03-09 22:50:26 +1100

    thinking about whether we need the SE class or not

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -7,32 +7,34 @@
 #' @slot molecules Slot for detected transcripts data.
 ###############################################################################
 
-.MoleculeExperiment <- setClass("MoleculeExperiment",
+.MoleculeExperiment <- setClass(Class = "MoleculeExperiment",
         # make class inherit properties from SummarizedExperiment class
-        contains = "SummarizedExperiment",
+        contains = c("SummarizedExperiment"),
         # define new slots
-        slots = representation(molecules = list()),
+        slots = c(molecules = "numeric")
 )
 
 
-# define validity checks
-#' @importFrom S4Vectors setValidity2
-S4Vectors:::setValidity2(class = 'MoleculeExperiment', .molecules_validity)
 
-.molecules_validity <- function(obj){
-    # define error messages to direct user to use valid input data
-    msg <- NULL
-    if(){
-        msg <- c(msg, "impose valid check specifications here")
-    }
-    # if object is valid, enable creation of class instance
-    if(is.null(msg)){
-        TRUE
-    }
-    else msg
-}
-
-
+###############################################################################
+## define validity checks
+##' @importFrom S4Vectors setValidity2
+#S4Vectors:::setValidity2(class = 'MoleculeExperiment', .molecules_validity)
+#
+#.molecules_validity <- function(obj){
+#    # define error messages to direct user to use valid input data
+#    msg <- NULL
+#    if(){
+#        msg <- c(msg, "impose valid check specifications here")
+#    }
+#    # if object is valid, enable creation of class instance
+#    if(is.null(msg)){
+#        TRUE
+#    }
+#    else msg
+#}
+#
+#
 #setValidity("MoleculeExperiment",
 ## if condition X
 ## print error message
--- R/MoleculeExperiment.R
+++ R/MoleculeExperiment.R
@@ -9,20 +9,24 @@
 
 # simple version for now
 
-MoleculeExperiment <- function(mdf, ...){
+MoleculeExperiment <- function(molecules, ...){
     # the ... argument enables us to pass arguments to the 
     # SummarizedExperiment constructor
 
     # do slot assigning for SummarizedExperiment class instance
     # me = molecule experiment
-    me <- SummarizedExperiment(list(mdf = mdf), ...)
+    me <- SummarizedExperiment(list(molecules = molecules), ...)
 
     # convert me obj to our class, via helper function .MoleculeExperiment
     .MoleculeExperiment(me)
+}
 
-    # alternative
-    #me <- new("MoleculeExperiment", molecules = mdf)
-    #return(me)
-    
+# do we even need the SE class?
+
+# simpler version that does NOT use the SummarizedExperiment class
+simple_me <- setClass("simple_me", slots = c(molecules = "numeric"))
+simple_me <- function(molecules){
+    me <- new("simple_me", molecules = molecules)
+    return(me)
 }
 
