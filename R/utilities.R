cf0f1461d04bf34073459ba742fee27b1316cab9
Nicholas Robertson <nickrrrr@gmail.com>
2023-03-29 16:55:40 +1100

    format utilities

--- R/utilities.R
+++ R/utilities.R
@@ -5,9 +5,11 @@
 # ------------------------------------------------------------------------------
 # get essential columns
 .get_essential_cols <- function(factor_col, x_col, y_col) {
-    essential_cols <- list(factor_col,
-                        x_col,
-                        y_col)
+    essential_cols <- list(
+        factor_col,
+        x_col,
+        y_col
+    )
     # check essential columns
 
     v <- unlist(lapply(essential_cols, is.null))
@@ -15,7 +17,6 @@
     if (isTRUE(any(v))) {
         stop("Essential columns have not been specified. Please specify column
 names in the three \"col\" arguments to this function")
-
     }
     return(unlist(essential_cols))
 }
@@ -24,14 +25,18 @@ names in the three \"col\" arguments to this function")
 # get standard columns depending on the slot
 .get_standard_cols <- function(df_type) {
     if (df_type == "transcripts") {
-        standard_cols <- c("feature_name",
-                            "x_location",
-                            "y_location")
+        standard_cols <- c(
+            "feature_name",
+            "x_location",
+            "y_location"
+        )
     }
     if (df_type == "boundaries") {
-        standard_cols <- c("segment_id",
-                            "x_location",
-                            "y_location")
+        standard_cols <- c(
+            "segment_id",
+            "x_location",
+            "y_location"
+        )
     }
     return(standard_cols)
 }
@@ -39,9 +44,8 @@ names in the three \"col\" arguments to this function")
 # ------------------------------------------------------------------------------
 # standardise column names across technologies
 .standardise_cols <- function(df,
-                                standard_cols,
-                                essential_cols) {
-
+                              standard_cols,
+                              essential_cols) {
     if (!identical(essential_cols, standard_cols)) {
         # get index for essential cols
         for (col in essential_cols) {
@@ -57,9 +61,8 @@ names in the three \"col\" arguments to this function")
 # -----------------------------------------------------------------------------
 # select specified columns
 .select_cols <- function(df,
-                            keep_cols,
-                            standard_cols) {
-    
+                         keep_cols,
+                         standard_cols) {
     if (keep_cols == "essential") {
         cols <- standard_cols
     } else if (keep_cols == "all") {
@@ -67,13 +70,13 @@ names in the three \"col\" arguments to this function")
     } else {
         cols <- keep_cols
         # check that essential columns have been selected too
-        for (c in standard_cols){
+        for (c in standard_cols) {
             if (!c %in% cols) {
                 stop("Essential columns could not be identified in the
 keep_cols argument. Essential columns are those specified in the \"col\"
 arguments of this function.")
             }
-        }   
+        }
     }
     return(cols)
 }
@@ -93,7 +96,6 @@ arguments of this function.")
 #' @importFrom magrittr %<>%
 #' @importFrom magrittr %>%
 .standardise_to_list <- function(df, cols, ...) {
-
     df %<>%
         dplyr::select(dplyr::all_of(cols)) %>%
         dplyr::group_by(dplyr::pick(...))
@@ -109,7 +111,6 @@ arguments of this function.")
 # -----------------------------------------------------------------------------
 # get sample IDs by retrieving name of parent directory
 .get_sample_id <- function(n_samples, f_paths) {
-
     ids <- vector("character", length = n_samples)
 
     for (f in seq_along(f_paths)) {
@@ -126,29 +127,45 @@ arguments of this function.")
 # ------------------------------------------------------------------------------
 # flatten ME list whilst retaining information of list headers
 .addColumnToNestedListFlatten <- function(listObject, column_name = NULL) {
-  listObject_added = mapply(function(df, nm) {
-    df[, column_name] <- nm
-    return(df)
-  }, listObject, names(listObject), SIMPLIFY = FALSE)
-  return(do.call(rbind, listObject_added))
+    listObject_added <- mapply(function(df, nm) {
+        df[, column_name] <- nm
+        return(df)
+    }, listObject, names(listObject), SIMPLIFY = FALSE)
+    return(do.call(rbind, listObject_added))
 }
 
 .flatten_molecules <- function(me, assay_name) {
-  molecules_flat <- .addColumnToNestedListFlatten(
-    lapply(me@molecules[assay_name], function(mol_2) .addColumnToNestedListFlatten(
-      lapply(mol_2, function(mol_1) .addColumnToNestedListFlatten(
-        mol_1, "feature_name")), "sample_id")),
-    column_name = NULL)
+    molecules_flat <- .addColumnToNestedListFlatten(
+        lapply(me@molecules[assay_name], function(mol_2) {
+            .addColumnToNestedListFlatten(
+                lapply(mol_2, function(mol_1) {
+                    .addColumnToNestedListFlatten(
+                        mol_1, "feature_name"
+                    )
+                }), "sample_id"
+            )
+        }),
+        column_name = NULL
+    )
 
-  return(molecules_flat)
+    return(molecules_flat)
 }
 
 .flatten_boundaries <- function(me, assay_name) {
-  molecules_flat <- .addColumnToNestedListFlatten(
-    lapply(me@boundaries[assay_name],
-        function(mol_2) .addColumnToNestedListFlatten(
-            lapply(mol_2, function(mol_1) .addColumnToNestedListFlatten(
-                mol_1, "segment_id")), "sample_id")),
-    column_name = NULL)
-  return(molecules_flat)
+    molecules_flat <- .addColumnToNestedListFlatten(
+        lapply(
+            me@boundaries[assay_name],
+            function(mol_2) {
+                .addColumnToNestedListFlatten(
+                    lapply(mol_2, function(mol_1) {
+                        .addColumnToNestedListFlatten(
+                            mol_1, "segment_id"
+                        )
+                    }), "sample_id"
+                )
+            }
+        ),
+        column_name = NULL
+    )
+    return(molecules_flat)
 }
