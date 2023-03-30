cd9fe038408ebe6e11ae4b04d55bbe0df5c13b76
Nicholas Robertson <nickrrrr@gmail.com>
2023-03-30 16:14:36 +1100

    add readBoundaries example

--- R/readBoundaries.R
+++ R/readBoundaries.R
@@ -27,7 +27,18 @@
 #' change the coordinate data from pixel to micron. It can be either a single
 #' integer, or multiple scale factors for the different samples. The default
 #' value is 1.
-#' 
+#' @examples
+#'   repo_dir <- system.file("extdata", package = "MoleculeExperiment")
+#'   nuclei_ls <- readBoundaries(data_dir = repo_dir,
+#'                               pattern = "nucleus_boundaries.csv",
+#'                               n_samples = 2,
+#'                               segment_id_col = "cell_id",
+#'                               x_col = "vertex_x",
+#'                               y_col = "vertex_y",
+#'                               keep_cols = "essential",
+#'                               boundaries_assay = "nucleus",
+#'                               scale_factor_vector = 0.2125)
+#'   nuclei_ls
 #' @export
 readBoundaries <- function(data_dir,
                             pattern = NULL,
