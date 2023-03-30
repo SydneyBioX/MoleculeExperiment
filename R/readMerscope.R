ba8298fe508dcd70d1e6c52a599841390e8fb912
Nicholas Robertson <nickrrrr@gmail.com>
2023-03-30 15:58:55 +1100

    document readMerscope

--- R/readMerscope.R
+++ R/readMerscope.R
@@ -1,14 +1,20 @@
 #' Read in Merscope data to an ME object
-#' 
+#'
 #' Reads in Merscope data (Vizgen) from a directory, and standardises it into
 #' a MoleculeExperiment object. It is essentially a wrapper around the function
 #' readMolecules(). Note that this function can currently only create a simple
 #' ME object with the molecules slot filled. Boundary information cannot be
 #' handled yet.
-#' 
-#' #TODO @param data_dir DESCRIPTION.
-#' #TODO @param n_samples DESCRIPTION. Defaults to 1.
-#' #TODO @param n_samples DESCRIPTION. Defaults to "essential".
+#'
+#' @param data_dir Character string specifying the directory with the Cosmx
+#'   output files.
+#' @param n_samples Integer specifying number of samples to be read.
+#'   Defaults to 1.
+#' @param keep_cols Vector of characters specifying the columns of interest from
+#'   the transcripts file. "essential" selects columns with gene names, x and y
+#'   locations. "all" will select all columns. Alternatively, specific colums
+#'   of interest can be selected by specifying them as characters in a vector.
+#'   Note that this personalised vector needs to contain the essential columns.
 #'
 #' @export
 
