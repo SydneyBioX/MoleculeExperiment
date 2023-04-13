28a1115393e72f7d59ce997dcc0c479ca4b84a84
barbarazpc <barbara.zita.peters@couto.de>
2023-04-13 14:56:47 +1000

    added backbone for unit testing

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -2,6 +2,11 @@
 # helper functions to reduce redundancy in unit tests
 # =============================================================================
 
-# constructor-like function to create me object
+# constructor-like function to create me object with example xenium data
+# it's not compute intensive, so can be run anew each time
 .new_me_obj <- function() {
+    repoDir <- system.file("extdata", package = "MoleculeExperiment")
+    me <- readXenium(repoDir,
+                        keepCols = "essential")
+    return(me)
 }
---
+++ tests/testthat/test-countMolecules.R
@@ -0,0 +1,6 @@
+
+test_that("terra leads to same results as sp",
+{
+
+}
+)
