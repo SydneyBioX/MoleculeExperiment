004576a0ae7cd99dcf31b189a5ecceb7a5dca18e
barbarazpc <barbara.zita.peters@couto.de>
2023-04-14 15:53:35 +1000

    countMolecules unit tests passed

--- tests/testthat/test-countMolecules.R
+++ tests/testthat/test-countMolecules.R
@@ -1,25 +1,60 @@
 test_that("check missing or invalid arguments raise an error", {
+    me <- .new_me_obj()
 
-}
-)
+    expect_error(countMolecules(me,
+                                moleculesAssay = NULL,
+                                segmentationInfo = "boundaries",
+                                boundariesAssay = "cell",
+                                matrixOnly = FALSE),
+                regexp = "not be NULL")
 
-test_that("stop() calls raise an error when prompted", {
-
-}
-)
-
-# TODO solve row/col sorting so that it is the same between terra and sp output
-test_that("terra implementation leads to same results as sp implementation", {
+    expect_error(countMolecules(me,
+                                moleculesAssay = 1,
+                                segmentationInfo = "boundaries",
+                                boundariesAssay = "cell",
+                                matrixOnly = FALSE),
+                regexp = "should be a character")
+    expect_no_error(countMolecules(me,
+                                moleculesAssay = "detected",
+                                segmentationInfo = "boundaries",
+                                boundariesAssay = "cell",
+                                matrixOnly = FALSE))
 
 }
 )
 
 test_that("output is of SpatialExperiment S4 class", {
+    me <- .new_me_obj()
 
+    spe <- suppressMessages(countMolecules(me,
+                            moleculesAssay = "detected",
+                            segmentationInfo = "boundaries",
+                            boundariesAssay = "cell",
+                            matrixOnly = FALSE))
+    expect_s4_class(me, "MoleculeExperiment")
+    expect_s4_class(spe, "SpatialExperiment")
+    expect_false(isTRUE(is(spe, "MoleculeExperiment")))
 }
 )
 
-test_that("output has expected number of cells", {
+test_that("terra implementation leads to same results as sp implementation", {
+    me <- .new_me_obj()
 
+    spe_terra <- suppressMessages(countMolecules(me,
+                            moleculesAssay = "detected",
+                            segmentationInfo = "boundaries",
+                            boundariesAssay = "cell",
+                            matrixOnly = FALSE))
+
+    spe_sp <- suppressMessages(countMolecules_sp(me,
+                            moleculesAssay = "detected",
+                            segmentationInfo = "boundaries",
+                            boundariesAssay = "cell",
+                            matrixOnly = FALSE))
+
+    counts_terra <- spe_terra@assays@data@listData$counts
+    counts_sp <- spe_sp@assays@data@listData$counts
+
+    expect_equal(as.matrix(counts_terra), as.matrix(counts_sp))
 }
 )
