b92025c06505f3838884f472f84761797f9f9e8d
Nicholas Robertson <nickrrrr@gmail.com>
2023-06-06 10:38:19 +1000

    remove sp dependency

--- tests/testthat/test-countMolecules.R
+++ tests/testthat/test-countMolecules.R
@@ -36,25 +36,3 @@ test_that("output is of SpatialExperiment S4 class", {
     expect_false(isTRUE(is(spe, "MoleculeExperiment")))
 }
 )
-
-test_that("terra implementation leads to same results as sp implementation", {
-    me <- .new_me_obj()
-
-    spe_terra <- suppressMessages(countMolecules(me,
-                            moleculesAssay = "detected",
-                            segmentationInfo = "boundaries",
-                            boundariesAssay = "cell",
-                            matrixOnly = FALSE))
-
-    spe_sp <- suppressMessages(countMolecules_sp(me,
-                            moleculesAssay = "detected",
-                            segmentationInfo = "boundaries",
-                            boundariesAssay = "cell",
-                            matrixOnly = FALSE))
-
-    counts_terra <- spe_terra@assays@data@listData$counts
-    counts_sp <- spe_sp@assays@data@listData$counts
-
-    expect_equal(as.matrix(counts_terra), as.matrix(counts_sp))
-}
-)
