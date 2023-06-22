9a6ad563602b09979f9cd7853b6e39d81c97ff10
Nicholas Robertson <nickrrrr@gmail.com>
2023-06-22 10:05:15 +1000

    fix affine transformation

--- load_CosMx_boundaries.R
+++ load_CosMx_boundaries.R
@@ -1,7 +1,7 @@
 library(terra)
-library(BiocParallel)
 library(cli)
 library(data.table)
+library(tidyverse)
 # 1. infer image topology ----
 
 data_dir <- "/albona/nobackup2/biostat/datasets/spatial/nanostring_NSCLC_lung9_rep1/modified/Lung9_Rep1/Lung9_Rep1-Flat_files_and_images"
@@ -32,8 +32,8 @@ poly_list <- lapply(
         xmin <- topology[i, 2][[1]]
         xmax <- topology[i, 2][[1]] + ncol(mask)
 
-        ymin <- topology[i, 3][[1]] - nrow(mask)
-        ymax <- topology[i, 3][[1]]
+        ymin <- topology[i, 3][[1]]
+        ymax <- topology[i, 3][[1]] + nrow(mask)
 
         terra::ext(mask) <- c(xmin, xmax, ymin, ymax)
         poly <- terra::as.polygons(mask, round = FALSE)
@@ -50,22 +50,17 @@ values(merged_vector) <- values(merged_vector) %>%
     dplyr::mutate(unique_cell_id = dplyr::row_number() - 1)
 
 
-x_min <- (23622 - 1733.33333333333) / 2
-x_max <- x_min + 5000
-y_min <- (151355.555555556 + 133107.555555556) / 2
-y_max <- ymin + 5000
-
-e <- terra::ext(x_min, x_max, y_min, y_max)
-
-transcripts <- data.table::fread(paste0(data_dir,
-                                        "/Lung9_Rep1_tx_file.csv"))
-nuclear <- transcripts[CellComp == "Nuclear"][,.(x_global_px, y_global_px)]
+transcripts <- data.table::fread(paste0(
+    data_dir,
+    "/Lung9_Rep1_tx_file.csv"
+))
+nuclear <- transcripts[CellComp == "Nuclear"][, .(x_global_px, y_global_px)]
 setnames(nuclear, c("x_global_px", "y_global_px"), c("x", "y"))
 idx <- sample(seq_len(nrow(nuclear)), 150000)
 mols <- terra::vect(as.matrix(nuclear[idx]))
 
 # plot the merged vector (VERY LARGE EDITION)
-png("merged.png", width = 12, height = 10, units = "in", res = 1800)
+png("merged.png", width = 8, height = 6, units = "in", res = 1200)
 plot(merged_vector)
-points(mols, col="pink", alpha = 0.1)
+points(mols, col = "pink", alpha = 0.1)
 dev.off()
