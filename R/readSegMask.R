23689756fc7a6b7accb9c5be1630653eb5f75bca
Nicholas Robertson <nickrrrr@gmail.com>
2023-06-26 15:44:27 +1000

    first draft of readSegMask

--- R/readSegMask.R
+++ R/readSegMask.R
@@ -1,5 +1,22 @@
-readSegMask <- function(path = NULL, image = NULL) {
+readSegMask <- function(
+    extent, path = NULL, image = NULL, assayName = "cell",
+    background_value = NULL, sample_id = NULL) {
     # Input validation.
+    e <- tryCatch(
+        terra::ext(extent),
+        error = function(err) {
+            cli::cli_abort(c(
+                "Invalid extent.",
+                "i" = paste0(
+                    "{.var extent} must be of the form",
+                    " c(xmin, xmax, ymin, ymax)."
+                ),
+                "x" = "From `terra::ext`:",
+                " " = "{err$message}"
+            ))
+        }
+    )
+
     if (is.null(path) && is.null(image)) {
         cli::cli_abort(c(
             "No valid mask was provided.",
@@ -41,5 +58,43 @@ readSegMask <- function(path = NULL, image = NULL) {
     }
 
     # TODO: How to figure out the extent of the image?
-    
+    ext(mask) <- e
+
+    geom_df <- as.data.frame(terra::geom(terra::as.polygons(mask)))
+
+    if (!is.null(background_value)) {
+        if (is.nan(background_value)) {
+            cli::cli_abort(c(
+                "Invalid background value:",
+                " " = "{.var background_value} is {background_value}.",
+                "i" = "Background value should be an integer."
+            ))
+        }
+        geom_df %<>%
+            dplyr::filter(
+                !.data[["geom"]] == background_value
+            )
+    }
+    geom_df %<>%
+        dplyr::mutate(
+            sample_id = ifelse(is.null(sample_id), "sample_1", sample_id)
+        ) %>%
+        dplyr::group_by(.data[["geom"]]) %>%
+        dplyr::filter(
+            dplyr::n_distinct(.data[["part"]]) < 1
+        ) %>%
+        dplyr::ungroup() %>%
+        dplyr::mutate(
+            # create ID col
+            cell_id = dplyr::consecutive_id(
+                .data[["sample_id"]], .data[["geom"]]
+            )
+        )
+
+    dataframeToMEList(
+        geom_df,
+        dfType = "boundaries", assayName = assayName,
+        sampleCol = "sample_id", factorCol = "cell_id", xCol = "x",
+        yCol = "y"
+    )
 }
