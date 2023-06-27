d0c129805c442054f6cdc892217af575053c1c76
Nicholas Robertson <nickrrrr@gmail.com>
2023-06-27 16:15:42 +1000

    initial readMerscope

--- R/readMerscope.R
+++ R/readMerscope.R
@@ -13,28 +13,79 @@
 #' locations. "all" will select all columns. Alternatively, specific colums
 #' of interest can be selected by specifying them as characters in a vector.
 #' Note that this personalised vector needs to contain the essential columns.
+#' @param addBoundaries A string with which to specify the name of the boundary
+#'     assay to be added to the me object. Can be a string, or NULL.
+#'     If NULL, a simple ME object with no boundaries will be created.
 #' @return A MoleculeExperiment object
 #' @export
 #' @examples
 #' repoDir <- system.file("extdata", package = "MoleculeExperiment")
 #' repoDir <- paste0(repoDir, "/vizgen_HumanOvarianCancerPatient2Slice2")
 #' meMerscope <- readMerscope(repoDir,
-#'                           keepCols = "essential")
+#'     keepCols = "essential"
+#' )
 #' meMerscope
 readMerscope <- function(dataDir,
-                         keepCols = "essential") {
+                         keepCols = "essential",
+                         addBoundaries = "cell") {
     # check arg validity
     .check_if_character(dataDir, keepCols)
 
     # create simple MoleculeExperiment object
     pattern <- "detected_transcripts.csv"
-    me <- readMolecules(dataDir = dataDir,
-                        pattern = pattern,
-                        featureCol = "gene",
-                        xCol = "global_x",
-                        yCol = "global_y",
-                        keepCols = keepCols)
+    me <- readMolecules(
+        dataDir = dataDir,
+        pattern = pattern,
+        featureCol = "gene",
+        xCol = "global_x",
+        yCol = "global_y",
+        keepCols = keepCols
+    )
 
+    sampleName <- "sample1"
+
+    if (!is.null(addBoundaries)) {
+        segFiles <- list.files(
+            path = dataDir, pattern = "*.hdf5", full.names = TRUE
+        )
+
+        df_all_list <- list()
+
+        for (segFile in segFiles) {
+            out <- rhdf5::h5read(segFile, "featuredata")
+            out <- lapply(out, "[[", "zIndex_0")
+            out <- lapply(out, "[[", "p_0")
+            out <- lapply(out, "[[", "coordinates")
+            out <- lapply(out, "[", , , 1)
+            out <- lapply(out, t)
+            dfList <- mapply(
+                function(nms, locations) {
+                    data.frame(
+                        x_location = locations[, 1],
+                        y_location = locations[, 2],
+                        cell_id = nms,
+                        sample_id = sampleName
+                    )
+                },
+                names(out), out,
+                SIMPLIFY = FALSE
+            )
+            df <- do.call(rbind, dfList)
+
+            df_all_list[[segFile]] <- df
+        }
+
+        df_all <- do.call(rbind, df_all_list)
+
+        me@boundaries <- MoleculeExperiment::dataframeToMEList(df_all,
+            dfType = "boundaries",
+            assayName = "cells",
+            sampleCol = "sample_id",
+            factorCol = "cell_id",
+            xCol = "x_location",
+            yCol = "y_location"
+        )
+    }
     # future development: handle complex hdf5 segmentation files
 
     return(me)
